3e8eb95b5a75061a5a306c1165a13bb0d59d9648
TeamDman <TeamDman9201@gmail.com>
2021-08-14 17:37:29 -0400

    Add gamemode page

--- upload.ps1
+++ upload.ps1
@@ -2,3 +2,13 @@ az storage blob upload-batch `
     --account-name "site10201" `
     --source .\content `
     --destination '$web' `
+
+$config = Get-Content .\.env | ConvertFrom-StringData;
+
+# https://www.davepaquette.com/archive/2020/05/10/deploying-a-static-site-to-azure-using-the-az-cli.aspx
+az cdn endpoint purge `
+    --resource-group $config.rgp `
+    --name $config.cdn `
+    --profile-name $config.profile `
+    --no-wait `
+    --content-paths '/' '/index.html';
