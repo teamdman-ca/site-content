83ea5106e1e3e327e657575b0218e2b930b70da0
TeamDman <TeamDman9201@gmail.com>
2022-06-30 15:16:55 -0400

    add trailing slash

--- teamdman.ca/app/src/routes/index.svelte
+++ teamdman.ca/app/src/routes/index.svelte
@@ -3,10 +3,10 @@
 	Welcome to my site.
 	<h1>Links</h1>
 	<ul>
-		<li><a href="syncplay">Public Syncplay server</a></li>
-		<li><a href="wordle">Cheating at wordle</a></li>
-		<li><a href="interesting">Interesting links</a></li>
-		<li><a href="vehicle-risk">Vehicle risk estimation tool</a></li>
+		<li><a href="syncplay/">Public Syncplay server</a></li>
+		<li><a href="wordle/">Cheating at wordle</a></li>
+		<li><a href="interesting/">Interesting links</a></li>
+		<li><a href="vehicle-risk/">Vehicle risk estimation tool</a></li>
 	</ul>
 </main>
 
