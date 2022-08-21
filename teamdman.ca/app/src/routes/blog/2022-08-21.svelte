fe334df412fb1d0f736cab3cc23f173e54ce75eb
TeamDman <TeamDman9201@gmail.com>
2022-08-21 01:18:21 -0400

    Add blog post.

---
+++ teamdman.ca/app/src/routes/blog/2022-08-21.svelte
@@ -0,0 +1,93 @@
+<article class="muh-main">
+	<h1>2022-08-21 1:18AM</h1>
+	<br />
+	<br />
+	There's something appealing about hidden information. In the Netflix true crime series
+
+	<aside>Crime Scene: The Vanishing at the Cecil Hotel 2021 ‧ Documentary ‧ 1 season</aside>
+
+	there's a girl who disappeared, and a bunch of internet stalkers crawled through every piece of
+	social media from the victim, which included a tumblr blog. Something about that has stuck with
+	me, probably from some unhealthy psychological complex? People caring about the blog posts of
+	someone, elevating them to some sort of celebrity status. Maybe it's like isekai, where the main
+	character is basically the ultimate self insert. True crime is interesting because it brings the
+	insanity of life a little closer to our closed off worlds.
+
+	<br />
+	<br />
+
+	There's a lot of stuff we all get exposed to in life, and I feel like I have some unique
+	experiences that would be good to share. The problem is that sharing this stuff in a way that
+	engages with others is challenging, some monkey part of my brain thinks of the cecil hotel series
+	and the main take away is that being murdered is a potentially effective method for getting blog
+	views.
+
+	<br />
+	<br />
+
+	There's something about mysterious websites that's also facinating. ARGs and other stuff. I have a
+	list of inspiration sites, which interestingly overlaps with my list of interesting sound design.
+	I want to make some AI music and sound effects using encoder-decoder stuff, but I'm too lazy.
+
+	<aside><a href="https://angusnicneven.com/Connect">example</a></aside>
+
+	<br />
+	For now I plan on keeping the blog unlinked from the index page. Only discoverable if you care enough
+	to use webpage discovery tools, or (more easily) if you just look at my github activity. It's liberating
+	to start this way, no need to opt into some kind of CMS, instead just typing whatever garbage I want
+	and barely formatting it. I anticipate this being read from github more than the actual website (if
+	ever read at all). At least the auto formatter I have adds line breaks so even the raw code will be
+	pretty readable.
+
+	<br />
+	<br />
+
+	The ARG / unknown theme of some of these neat websites would definitely be negated if the whole
+	website was open source like I plan to keep mine. I think there's still plenty of ways I can
+	explore websites as an art form, leaving the choice of peeking beyond the curtain to any who
+	really want to.
+
+	<br />
+	<br />
+	I think it was the Cortex podcast where CGPGrey recently mentioned reading letters to yourself they
+	had previously written. I don't think I have any of those, but I can look at git commits from some
+	of my earliest stuff, some old screenshots I have, and it forms a kind of scrapbook but still doesn't
+	have everything that the letter-to-self kinda encapsulates. Having to compress ideas to keep them digestible
+	but without wasting the reader's time is quite a skill.
+
+	<br />
+	<br />
+	<div class="bg-slate-800">
+		<h2>Letter to myself</h2>
+		Dear me,
+		<br />
+		I still have yet to make a video essay, but I want to pursue youtube as a content creator more. I
+		have a list of projects that I want to implement already, but for some reason I don't find the effort
+		to do any of them now that I don't have school projects as an excuse? I expect future me to have
+		created at least one high quality youtube video longer than 5 minutes. I created the Blind Guardian
+		- Battlefield music video during summer vacation, but that doesn't quite count. If not youtube, then
+		I expect progress on SuperFactoryManager at least. There's something voyeuristic about writing this
+		where I know others can see. I wonder if the self-filtering that comes with knowing this is public
+		devalues the theraputic effects that are associated with journaling? Currently going through Jacob
+		Geller's patreon content, great stuff.
+	</div>
+	<br />
+
+	I wonder if this will inspire more blog posts now that I've broken the ice, or if this is doomed
+	to hiatus like the isekai story I want to write. At least I can consider this a completed item, I
+	tried creating a video on text-to-image software but basically gave up. The Vox video is so good,
+	I don't have any concrete thoughts to make a video. Will probably create a page on this website
+	instead at some point. Much easier to play to my strengths, but I still want a video for some
+	reason.
+
+	<br />
+	<br />
+
+	I quite literally expect nobody to see this, but am still procrastinating a closing sentence? I'm
+	also not confident I won't relent at some point and link to these in a more discoverable way.
+
+	<br />
+	<br />
+
+	Haaaaaaaaave a good one, peace.
+</article>
