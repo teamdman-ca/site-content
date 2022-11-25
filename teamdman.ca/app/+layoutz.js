af3e5aa95ce6bb4fdf6bd1f20a39845401cd41f5
TeamDman <TeamDman9201@gmail.com>
2022-11-25 00:57:07 -0500

    prep for migration

---
+++ teamdman.ca/app/+layoutz.js
@@ -0,0 +1,10 @@
+/** @type {import('@sveltejs/kit').KitConfig} */
+export default {
+    prerender: {
+        default: true,
+    },
+    trailingSlash: 'always',
+}
+// export const scr = false;
+// export const prerender = true;
+// export const trailingSlash = "";
