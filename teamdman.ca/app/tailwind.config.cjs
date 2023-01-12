1361cee6c8ab265dc9b04e40fb012bd19722fa20
TeamDman <TeamDman9201@gmail.com>
2023-01-12 17:49:29 -0500

    update svelte

--- teamdman.ca/app/tailwind.config.cjs
+++ teamdman.ca/app/tailwind.config.cjs
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./src/**/*.{html,js,svelte,ts}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("@tailwindcss/typography")],
-	darkMode: "media",
-};
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography")
+  ],
+}
