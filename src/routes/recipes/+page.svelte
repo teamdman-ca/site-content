8faf675e20dabd0c0fe4692748d30cdd2df54ed1
TeamDman <TeamDman9201@gmail.com>
2023-12-30 22:52:39 -0500

    Add turkey soup recipe

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -24,6 +24,14 @@
 					</h2>
 				</li>
 			{/each}
+			<li>
+				<h2>
+					<a class="underline" href="momdadturkeysoup"
+						>MomDad Turkey Soup
+						<img src="momdadturkeysoup/1.jpg" alt="recipe preview" />
+					</a>
+				</h2>
+			</li>
 		</ul>
 	</article>
 </main>
